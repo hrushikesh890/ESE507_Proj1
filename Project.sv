02a6beeac09f72d26cb4fe0605d600af422d839e
Hrushikesh Patil <hrushikeshpatil890@gmail.com>
2019-09-20 19:18:28 -0400

    Control Module

--- Project.sv
+++ Project.sv
@@ -1,21 +1,39 @@
-module dff (clk, reset, a, valid_in, dff_out)
-	input clk, reset, valid_in;
+module dff (clk, reset, a, enable_a, dff_out)
+	input clk, reset, enable_a;
 	input [7:0] a; 
 	output logic [7:0] dff_out;
 
 	always_ff @(posedge clk) begin
 		if (reset == 1)
 			dff_out <= 0;
-		else if ((reset == 0) && (valid_in = 1))
+		else if ((reset == 0) && (enable_a = 1))
 			dff_out <= a;
 		else if (valid == 0)
 			dff_out <= 0;
 		else
 			dff_out <= 0;
+	end
+end
 
 module control (valid_in, valid_out, enable_a, enable_f)
+	input valid_in;
+	output logic valid_out, enable_a, enable_f;
 
-module part2(clk, reset, a, valid_in, f,valid_out);
+	always_comb begin
+		if (valid_in == 1)
+			enable_a = 1;
+			enable_f = 1;
+			valid_out = 1;
+		else
+			enable_a = 0;
+			enable_f = 0;
+			valid_out = 0;
+	end
+end
+		
+
+
+module part2(clk, reset, a, valid_in, f, valid_out);
 	input clk, reset, valid_in;
 	input [7:0] a; 
 	output logic [19:0] f;
